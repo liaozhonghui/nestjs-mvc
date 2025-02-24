a1ba7416451b968d317a5039215be4b70aac886d
zhonghui <liaozhonghui@playdayy.com>
2025-02-24 21:01:02 +0800

    style(remove code def): remove custome code def;

--- src/common/errors/custom.error.ts
+++ src/common/errors/custom.error.ts
@@ -21,19 +21,12 @@ export enum ERROR_CODE {
   USER_HAS_BIND = 602, // 用户已绑定
   USER_HAS_DELETED = 603, // 用户已删除
 
-  OSS_SERVICE_ERROR = 701, // oss服务错误
   UNKNOWN = 999, // 未知错误
 }
 
 export class ILLegalArgumentException extends CustomError {
   constructor(msg: string) {
     super(ERROR_CODE.ILLEGAL_ARGUMENT, msg);
-  }
-}
-
-export class OSSUploadException extends CustomError {
-  constructor(msg: string) {
-    super(ERROR_CODE.OSS_SERVICE_ERROR, msg);
   }
 }
 
@@ -58,57 +51,6 @@ export enum CODE {
   DEVICE_LIMIT_TOUCH = 606, // 设备数达到上限,登录设备为5个，重新登录
   DEVICE_IS_DELETED = 607, // 设备已注销
   GOOGLE_ACCOUNT_TOKEN_VERIFY_ERROR = 608, // 设备已注销
-
-  /**
-   * 基本页面：首页，播放页，追剧，我的，充值页
-   */
-  COLLECT_BOOK_OFFLINE = 610, // 收藏时，短剧已下架
-  COLLECT_PARAM_ERROR = 611, // 收藏时，传参错误，chapter_id和book_id对不上.
-  LACK_REGION_PLAN = 612, // 缺少首页方案(app端显示无数据)
-  LACK_GIFT_GROUP = 613, // 缺少充值档位分组
-  RECHARGE_REPEAT = 614, // 订单已经充值过了
-  INSUFFICIENT_BALANCE = 615, // 余额不足
-  BOOK_CHAPTER_UPDATED = 616, // 卡点计费，短剧信息更新了
-  RECHARGE_TIMES_LIMIT = 617, // 订单已经到达充值次数限制
-  TL_GIFT_EXPIRED = 618, // 限时礼包已经过期
-
-  /**
-   * 支付订单
-   */
-  PAY_API_ERROR = 620, // 支付接口调用失败
-  APPLE_VERIFY_ERROR = 621, // 苹果支付验证失败
-  ANDROID_VERIFY_ERROR = 622, // 谷歌支付验证失败
-  RECHARGE_PARAMS_ERROR = 623, // 充值参数错误
-  APPLE_SUBSCRIPTION_USED = 624, // 苹果订阅已被其他账号占用
-
-  STARTUP_BOOK_NULL = 630, // 启动时，短剧不存在
-
-  /**
-   * 广告解锁
-   */
-  VIEW_AD_TIMES_NOT_ENOUGH = 631, // 用户看的广告数未达到解锁条件
-  NO_MATCHED_AD_CATEGORY = 632, // 没有匹配到用户分层
-  AD_UNLOCK_NOT_AVALIABLE = 633, // 当前剧集不能使用广告解锁
-  AD_UNLOCK_TIMES_EXCEEDED = 634, // 今日广告解锁次数已用完
-
-  /**
-   * 任务系统
-   */
-  TASK_NOT_FOUND = 640, // 任务不存在
-  TASK_REWARDED = 641, // 任务奖励已经领取
-  EMAIL_EXISTED = 642, // 邮箱已被占用
-  TASK_ACROSS_DATE = 643, // 任务不能跨天领取
-  TASK_NOT_COMPLETED = 644, // 任务未完成
-  TASK_RECEIVED = 645, // 任务已经领取
-  CHECKIN_TASK_AD_INVALID = 646, // 签到看广告跨天了，此次上报无效
-  EMAIL_TOO_MANY_TIMES = 647, // 邮箱已发送过太多次
-  EMAIL_DURATION_TOO_SHORT = 648, // 邮箱已发送过太短的时间
-  EMAIL_SEND_ERROR = 649, // 邮件发送异常
-
-  /**
-   * 分享
-   */
-  SHARE_BOOK_NOT_EXIST = 660, // 分享的短剧不存在
 
   /**
    * 订阅
